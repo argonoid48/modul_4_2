34d244f92192fa626fd6298fca14ac02b58cb08c
argonoid48 <158807187+argonoid48@users.noreply.github.com>
2024-11-14 21:11:20 +0300

    Add files via upload

---
+++ module_4_2.py
@@ -0,0 +1,18 @@
+# Галкин Андрей
+# Домашняя работа по уроку "Пространство имен."
+
+
+
+def test_function():
+
+    def inner_function():
+        print("Я в области видимости функции test_function")
+
+    inner_function()
+
+
+
+test_function()
+inner_function()  # Ошибка
+
+
